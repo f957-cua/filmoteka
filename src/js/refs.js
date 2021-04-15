a40d31874733d5231e7d039cb49518d6e3753f25
KalVasFlame <unnameddwar@gmail.com>
2021-04-15 18:34:52 +0300

    +

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,9 @@
 const refs = {
   body: document.body,
   logo: document.querySelector('.js-logo'),
-  gallery: document.querySelector('.js-fetch-list')
+  gallery: document.querySelector('.js-fetch-list'),
+  headerSearchInput: document.querySelector('.js-input-search'),
+  headerSearchBtn: document.querySelector('.js-btn-submit'),
 }
 
 export default refs;
