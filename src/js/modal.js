05f4796eb8bc18b0494a00cd23e3525b69e7e11b
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-04-23 18:23:01 +0300

    поправки модалки

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,20 +5,64 @@ import filmInfo from '../templates/film-info.hbs';
 refs.gallery.addEventListener('click', onFilmCardClick)
 refs.btnModalClose.addEventListener('click', closeModal)
 refs.filmModal.addEventListener('click', onBdpClick)
+refs.sliderContainer.addEventListener('click', onSlideCardClick)
 window.addEventListener('keydown', onEscCloseModal)
+
+let filmId = null;
+let isCard = null;
 
 function onFilmCardClick(e) {
     refs.filmInfoContainer.innerHTML = ''
-    const isCard = e.target.closest('.photo-card');    
+    isCard = e.target.closest('.photo-card');    
     if (!isCard) {
         return
     }
     let filmId = isCard.getAttribute('data')
+    let dataImg = isCard.getAttribute('data-img')
+    let dataTitle = isCard.getAttribute('data-title')
+    let dataPop = isCard.getAttribute('data-pop')
+    let dataVA = isCard.getAttribute('data-v_a')
+    let dataVC = isCard.getAttribute('data-v_c')
+    let dataOverview = isCard.getAttribute('data-overview')
+
+    apiService.getById(filmId).then((res) => {
+        if (!res.poster_path) {
+            res.poster_path = dataImg
+        }
+        if (!res.title) {
+            res.title = dataTitle
+        }
+        if (!res.popularity) {
+            res.popularity = dataPop
+        }
+        if (!res.original_title) {
+            res.original_title = dataTitle
+        }
+        if (!res.overview) {
+            res.overview = dataOverview
+        }
+        if (!res.vote_average) {
+            res.vote_average = dataVA
+        }
+        if (!res.vote_count) {
+            res.vote_count = dataVC
+        }
+      refs.filmInfoContainer.innerHTML = filmInfo(res)  
+    }
+        )    
+    refs.filmModal.classList.remove('is-hidden');    
+}
+
+function onSlideCardClick(e) {
+    refs.filmInfoContainer.innerHTML = ''
+    isCard = e.target.closest('.card-slider');    
+    if (!isCard) {
+        return
+    }
+    filmId = isCard.getAttribute('data-action')
     apiService.getById(filmId).then((res) => 
         refs.filmInfoContainer.innerHTML = filmInfo(res))    
-    refs.filmModal.classList.remove('is-hidden');
-    
-    
+    refs.filmModal.classList.remove('is-hidden');    
 }
 
 function closeModal() {
