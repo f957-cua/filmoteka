44e92ae69807585546fd424a484269c5c4bd6b14
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-04-25 11:52:44 +0300

    Подчистил файлы, создал my-libery.js

---
+++ src/js/my-libery.js
@@ -0,0 +1,13 @@
+import { ui, uiConfig, writeUserData, readUserData } from './firebase';
+import { openCloseModal } from './registration-helpers';
+import refs from './refs';
+
+refs.myLibrary.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (refs.registrationBtn.textContent === 'Sign in') {
+    openCloseModal();
+    ui.start('#firebaseui-auth-container', uiConfig);
+    return;
+  }
+  readUserData();
+});
---
+++ src/js/registration-helpers.js
@@ -0,0 +1,47 @@
+import refs from './refs';
+import { signOut } from './firebase';
+
+function openCloseModal() {
+  refs.registrationModal.classList.toggle('is-hidden');
+
+  if (refs.registrationModal.classList.contains('is-hidden')) { 
+    window.removeEventListener('keydown', onEscCloseRegModal);
+    refs.registrationModal.querySelector('.modal-close')
+    .removeEventListener('click', openCloseModal);
+    refs.registrationModal.removeEventListener('click', onBdpRegClick);
+    return;
+  }
+
+    window.addEventListener('keydown', onEscCloseRegModal);
+    refs.registrationModal.querySelector('.modal-close')
+    .addEventListener('click', openCloseModal);
+    refs.registrationModal.addEventListener('click', onBdpRegClick);
+}
+
+function onBdpRegClick(e) {
+  if (e.target !== refs.registrationModal) {
+      return
+  }
+
+  openCloseModal()
+}
+
+function onEscCloseRegModal(e) {
+  if (e.code === 'Escape') openCloseModal()
+}
+
+function signedUser(url, name) {
+  refs.registrationBtn.textContent = 'Sign out';
+  refs.registrationBtn.style.marginRight = "0px";
+  refs.registrationBtn.addEventListener('click', signOut);
+  refs.accountInfo.insertAdjacentHTML("afterbegin",`<img  class="user-img" src= ${url} alt= ${name} width="48px">`);
+}
+
+function noSignedUser() {
+  refs.registrationBtn.textContent = 'Sign in';
+  refs.registrationBtn.style.marginRight = "60px";
+  refs.accountInfo.textContent = "";
+  refs.registrationBtn.removeEventListener('click', signOut);
+}
+
+export { signedUser, noSignedUser, onEscCloseRegModal }
