6b17eed61a60f08625b1edeeca689281183ab6e4
VVariychuk <71844197+VVariychuk@users.noreply.github.com>
2021-04-30 18:42:26 +0300

    добавление трейлера на модаку слайдера

--- src/js/modal.js
+++ src/js/modal.js
@@ -30,7 +30,7 @@ function onFilmCardClick(e) {
     }).then(id => {
         apiService.getTrailerById(id).then(({ results }) => {
             const imgRef = document.querySelector('.film-poster-wrapper');
-            imgRef.addEventListener('click', e => {
+            imgRef.addEventListener('click', () => {
                 const markup = trailerHbs(results[0])
                 refs.trailerModal.innerHTML = markup
                 refs.trailerModal.classList.remove('is-close')
@@ -40,8 +40,11 @@ function onFilmCardClick(e) {
         })
     }).catch(console.log);
     refs.filmModal.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
 }
-    function onSlideCardClick(e) {
+
+
+function onSlideCardClick(e) {
         refs.filmInfoContainer.innerHTML = ''
         isCard = e.target.closest('.card-slider');
         if (!isCard) {
@@ -52,13 +55,27 @@ function onFilmCardClick(e) {
             modalValidation(res)
             refs.filmInfoContainer.innerHTML = filmInfo(res);
             return res;
-        }).then(addListenerOnBtnModal).catch(console.log);
+        }).then(res => {
+        addListenerOnBtnModal(res)
+        return res.id
+    }).then(id => {
+        apiService.getTrailerById(id).then(({ results }) => {
+            const imgRef = document.querySelector('.film-poster-wrapper');
+            imgRef.addEventListener('click', () => {
+                const markup = trailerHbs(results[0])
+                refs.trailerModal.innerHTML = markup
+                refs.trailerModal.classList.remove('is-close')
+                refs.trailerBackdrop.classList.remove('is-close')
+
+            })
+        })
+    }).catch(console.log);
         refs.filmModal.classList.remove('is-hidden');
         document.body.style.overflow = 'hidden';
 
     }
 
-    function closeModal() {
+function closeModal() {
         const isClosed = refs.filmModal.classList.contains('is-hidden')
         if (isClosed || !refs.trailerBackdrop.classList.contains('is-close')) {
             return
@@ -68,36 +85,37 @@ function onFilmCardClick(e) {
         return refs.filmModal.classList.add('is-hidden')
     }
 
-    function onBdpClick(e) {
+function onBdpClick(e) {
         if (e.target !== refs.filmModal) {
             return
         }
         closeModal()
     }
 
-    function onEscCloseModal(evt) {
+function onEscCloseModal(evt) {
         if (evt.code === 'Escape') {
             return closeModal()
         }
     }
 
-    function closeTrailerModal() {
+function closeTrailerModal() {
         refs.trailerBackdrop.classList.add('is-close')
         refs.trailerModal.innerHTML = '';
     
     }
 
-    function onCloseTrailerModal(e) {
+function onCloseTrailerModal(e) {
         if (e.code === 'Escape' || e.target.classList.contains('js-trailer-backdrop')) {
             closeTrailerModal()
         }
-    }
-    window.addEventListener('keydown', onCloseTrailerModal)
+}
+    
+window.addEventListener('keydown', onCloseTrailerModal)
 
-    refs.trailerBackdrop.addEventListener('click', onCloseTrailerModal)
+refs.trailerBackdrop.addEventListener('click', onCloseTrailerModal)
 
 
-    function modalValidation(res) {
+function modalValidation(res) {
         let dataImg = isCard.getAttribute('data-img')
         let dataTitle = isCard.getAttribute('data-title')
         let dataPop = isCard.getAttribute('data-pop')
@@ -130,4 +148,4 @@ function onFilmCardClick(e) {
         if (!res.genres) {
             res.genres = dataGenres.split(' ').map(el => { return { name: el } })
         }
-    }
+}
